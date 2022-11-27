529dc04b636bd90d3c760ef8eb258e682b05d6ea
GabrielJonatas <64110289+GabrielJonatas@users.noreply.github.com>
2022-11-26 21:37:12 -0300

    Add files via upload

---
+++ Exercicio3 - Estrutura sequencial/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int A,B,C,D,DIFERENCA;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		sc.close();
+		
+		DIFERENCA = (A*B) - (C*D);
+		
+		Locale.setDefault(Locale.US);
+		System.out.println("Diferenca = " + DIFERENCA);
+	}
+
+}
